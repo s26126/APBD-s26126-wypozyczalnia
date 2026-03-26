9b574d94462d4e8479a6647d455937b89c68a79a
Kacper Tomaszewski <s26126@pjwstk.edu.pl>
2026-03-26 23:19:18 +0100

    Implementacja podziału na typy użytkowników i aktualizacja modelu User

--- APBD-s26126-wypozyczalnia/Models/Users/Employee.cs
+++ APBD-s26126-wypozyczalnia/Models/Users/Employee.cs
@@ -2,9 +2,10 @@ namespace APBD_s26126_wypozyczalnia.Models;
 
 public class Employee : User
 {
+    public override int MaxRentals => 5;
+    public override UserType Type => UserType.Employee;
+
     public Employee(string firstName, string lastName) : base(firstName, lastName)
     {
-        
-        
     }
 }
