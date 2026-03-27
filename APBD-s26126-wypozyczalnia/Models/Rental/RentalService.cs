99cde0c7e04833b301c7746c349af9033f8fc7a3
s26126 <s26126@pjwstk.edu.pl>
2026-03-27 17:30:45 +0100

    Implementacja metod AddUser i AddEquipment oraz inicjalizacja testowych danych

--- APBD-s26126-wypozyczalnia/Models/Rental/RentalService.cs
+++ APBD-s26126-wypozyczalnia/Models/Rental/RentalService.cs
@@ -1,6 +1,52 @@
 namespace APBD_s26126_wypozyczalnia.Models.Rental;
+using APBD_s26126_wypozyczalnia.Models.Equipment;
 
 public class RentalService
 {
+    private List<User> _users = new List<User>();
+    private List<Equipment> _equipment = new List<Equipment>();
+    private List<Rental> _rentals = new List<Rental>();
+    
+    public void AddUser(User user)
+    {
+        _users.Add(user);
+    }
+    public void AddEquipment(Equipment equipment)
+    {
+        _equipment.Add(equipment);
+    }
+    public void PrintAllUsers()
+    {
+        Console.WriteLine("--- ZAREJESTROWANI UŻYTKOWNICY ---");
+        _users.ForEach(Console.WriteLine);
+    }
+    public void PrintAllEquipment()
+    {
+        Console.WriteLine("\n--- ZAREJESTROWANY SPRZĘT ---");
+        _equipment.ForEach(Console.WriteLine);
+    }
+    
+    public void PrintAvailableEquipment()
+    {
+        Console.WriteLine("\n--- DOSTĘPNE SPRZĘT ---");
+        foreach (var e in GetAvailableEquipment())
+        {
+            Console.WriteLine(e);
+        }
+    }
+    public List<Equipment> GetAvailableEquipment()
+    {
+        return _equipment.Where(e => e.Status == EquipmentStatus.Available).ToList();
+    }
+
+    public void AddRental(User user, Equipment equipment)
+    {
+        if (user == null || equipment == null)
+        {
+            throw new ArgumentException("User i equipment nie może być null");
+        }
+        
+        _rentals.Add(new Rental(user, equipment));
+    }
     
 }
--- APBD-s26126-wypozyczalnia/Program.cs
+++ APBD-s26126-wypozyczalnia/Program.cs
@@ -1,3 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using APBD_s26126_wypozyczalnia.Models;
+using APBD_s26126_wypozyczalnia.Models.Equipment;
+using APBD_s26126_wypozyczalnia.Models.Rental;
 
-Console.WriteLine("Hello, World!");
+namespace APBD_s26126_wypozyczalnia;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var service = new RentalService();
+
+        var usr1 = new Student("Jan", "Kowalski");
+        var usr2 = new Student("Anna", "Nowak");
+        var usr3 = new Student("Piotr", "Zieliński");
+        var usr4 = new Student("Maria", "Wiśniewska");
+        var usr5 = new Student("Tomasz", "Lewandowski");
+        service.AddUser(usr1);
+        service.AddUser(usr2);
+        service.AddUser(usr3);
+        service.AddUser(usr4);
+        service.AddUser(usr5);
+
+        var usr6 = new Employee("Adam", "Mickiewicz");
+        var usr7 = new Employee("Wisława", "Szymborska");
+        var usr8 = new Employee("Czesław", "Miłosz");
+        var usr9 = new Employee("Olga", "Tokarczuk");
+        var usr10 = new Employee("Juliusz", "Słowacki");
+        service.AddUser(usr6);
+        service.AddUser(usr7);
+        service.AddUser(usr8);
+        service.AddUser(usr9);
+        service.AddUser(usr10);
+
+        var eq1 = new Laptop("Dell XPS 13", "Intel i7", 16, 512, "Windows 11");
+        var eq2 = new Laptop("MacBook Air M2", "Apple M2", 8, 256, "macOS");
+        var eq3 = new Laptop("Lenovo ThinkPad X1", "Intel i5", 16, 512, "Ubuntu 22.04");
+        var eq4 = new Laptop("Asus ROG Zephyrus", "AMD Ryzen 9", 32, 1024, "Windows 11");
+        var eq5 = new Camera("Sony A7 III", 24);
+        var eq6 = new Camera("Canon EOS R6", 20);
+        var eq7 = new Camera("Nikon Z6 II", 24);
+        var eq8 = new Projector("Epson EB-L210W", 60, 4500);
+        var eq9 = new Projector("BenQ TK700STi", 120, 3000);
+        var eq10 = new Projector("Optoma UHD38", 240, 4000);
+        service.AddEquipment(eq1);
+        service.AddEquipment(eq2);
+        service.AddEquipment(eq3);
+        service.AddEquipment(eq4);
+        service.AddEquipment(eq5);
+        service.AddEquipment(eq6);
+        service.AddEquipment(eq7);
+        service.AddEquipment(eq8);
+        service.AddEquipment(eq9);
+        service.AddEquipment(eq10);
+
+
+
+        // service.PrintAllUsers();
+        // service.PrintAllEquipment();
+
+
+
+
+        service.PrintAvailableEquipment();
+
+        service.AddRental(usr1, eq1);
+        service.AddRental(usr2, eq2);
+        service.AddRental(usr3, eq3);
+        service.AddRental(usr4, eq4);
+        service.AddRental(usr5, eq5);
+
+        service.PrintAvailableEquipment();
+
+    }
+}
+    
+
